f99a8807eb7cab03be73668d10731e7eff5318a4
eclonit <davizinhozl.mendes@gmail.com>
2019-03-21 13:48:52 -0300

    atualização 5

--- KotlinApplication/app/src/main/java/com/example/aluno/kotlinapplication/AppStaticServices.kt
+++ KotlinApplication/app/src/main/java/com/example/aluno/kotlinapplication/AppStaticServices.kt
@@ -3,7 +3,7 @@ package com.example.aluno.kotlinapplication
 class AppStaticServices {
     companion object{
         public fun login(userStr: String, passStr: String): Boolean{
-            return if(userStr.equals("admin") && passStr.equals("admin123")) true else false;
+            return if(userStr.equals("admin") && passStr.equals("admin123")) true else false
         }
     }
 }
--- KotlinApplication/app/src/main/java/com/example/aluno/kotlinapplication/MainActivity.kt
+++ KotlinApplication/app/src/main/java/com/example/aluno/kotlinapplication/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.aluno.kotlinapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.util.Log;
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import org.jetbrains.anko.toast
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,36 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val enviarBtn: Button = findViewById<Button>(R.id.enviarBtn);
-        val userTxt: TextView = findViewById<TextView>(R.id.userTxt);
-        val passTxt: TextView = findViewById<TextView>(R.id.passTxt);
-        val userEditText: EditText = findViewById<EditText>(R.id.userEditText);
-        val passEditText: EditText = findViewById<EditText>(R.id.passEditText);
+        val enviarBtn: Button = findViewById<Button>(R.id.enviarBtn)
+        val userEditText: EditText = findViewById<EditText>(R.id.userEditText)
+        val passEditText: EditText = findViewById<EditText>(R.id.passEditText)
 
-        userTxt.x = 70f;
-        userTxt.y = 70f;
+        userEditText.y = 0f
 
-        userEditText.x = 150f;
-        userEditText.y = 40f;
+        passEditText.y = 60f
 
-        passTxt.x = 80f;
-        passTxt.y = 110f;
-
-        passEditText.x = 150f;
-        passEditText.y = 85f;
-
-        enviarBtn.x = 150f;
-        enviarBtn.y = 180f;
+        enviarBtn.y = 120f
 
         enviarBtn.setOnClickListener{
-            var loginBoolValue: Boolean =
-                    AppStaticServices.login(userEditText.getText().toString(), passEditText.getText().toString());
+            val loginBoolValue: Boolean =
+                    AppStaticServices.login(userEditText.getText().toString(), passEditText.getText().toString())
 
             if(loginBoolValue)
-                toast("Bem-vindo administrador.");
+                toast("Bem-vindo administrador.")
 
             else
-                toast("usuário/senha incorretos.");
-        };
+                toast("usuário/senha incorretos.")
+        }
     }
 }
